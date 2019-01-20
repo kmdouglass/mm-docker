6e45099e3b2099f3576f40e6e7ec42166cc20a27
kmdouglass <kyle.m.douglass@gmail.com>
2019-01-20 11:28:25 +0100

    Added build image for the libMMCore.a static library

---
+++ libMMCore-ubuntu-16.04-x86_64/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set +o errexit
+
+cd /micro-manager/micro-manager
+./autogen.sh
+./configure \
+    --prefix="/micro-manager/build" \
+    --with-boost-libdir="/usr/lib/x86_64-linux-gnu" \
+    --with-boost="/usr/include/boost" \
+    --disable-java-app \
+    --disable-install-dependency-jars \
+    --with-java="no"
+
+cd MMCore
+make -j "${NUM_CORES}"
+mkdir -p /micro-manager/build/lib/micro-manager
+cp .libs/libMMCore.a /micro-manager/build/lib/micro-manager
